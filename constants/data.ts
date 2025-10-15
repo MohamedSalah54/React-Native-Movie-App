3ac6734d909e49abb5808f290c6aad1d60b07eaa
MohamedSalah54 <mohamed.salah199988.com>
2025-10-15 18:42:23 +0300

    first init

---
+++ constants/data.ts
@@ -0,0 +1,15 @@
+  export const data = [
+    {
+        type: 'clothes',
+        data: ['shoes', 'skirt','pant','sunblock']
+    },
+    {
+        type: 'fruits',
+        data: ['banana', 'apple','watermilon','strwabery']
+    },
+    {
+        type: 'vegetables',
+        data: ['carot', 'tomatos','garlick','onion']
+    },
+ 
+  ];
