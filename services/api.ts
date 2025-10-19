a1ab1ad8e67d8649a8eb1ba02ae31c95241319d3
MohamedSalah54 <mohamed.salah199988.com>
2025-10-19 17:07:59 +0300

    movie details page

--- services/api.ts
+++ services/api.ts
@@ -28,3 +28,25 @@ export const fetchMovies = async ({
   const data = await response.json();
   return data.results;
 };
+
+export const fetchMovieDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    const res = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
+        method: "GET",
+        headers: TMDB_CONFIG.headers,
+      }
+    );
+    if (!res.ok) {
+      throw new Error("Failed to fetch movie details");
+    }
+    const data = await res.json()
+    return data
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
